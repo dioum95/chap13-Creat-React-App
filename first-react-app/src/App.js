b32e4ff5fe00a636a0c48c4105565d130121e2e8
dioum95 <cheikhmbackedioum95@gmail.com>
2023-11-16 18:40:41 +0100

    first commit

---
+++ first-react-app/src/App.js
@@ -0,0 +1,13 @@
+import{Clock} from './Clock'; 
+import './clock.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Clock/>
+    </div>
+  );
+}
+
+export default App;
